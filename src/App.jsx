3debea6718212633d6e7d9c95d6ced38a1c4efbe
Pavitrank <pavitrank2002@gmail.com>
2025-05-07 08:26:31 +0530

    "feat: Added Home compoennt to App file"

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { BrowserRouter, Router, Routes, Route } from "react-router-dom";
+import Home from "../src/Components/Home";
 
 const App = () => {
-  return <p>This is the App component</p>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
