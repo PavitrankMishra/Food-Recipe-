da45363e1a63625184b5bc9187c196f7f65656e1
Pavitrank <pavitrank2002@gmail.com>
2025-05-07 10:01:43 +0530

    "style" Added new heading for home container"

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
 import "./Home.css";
-import Header from './Header';
+import Header from "./Header";
 
 const Home = () => {
   return (
     <>
-        <Header />
+      <Header />
+      <section className="heroSection">
+        {/* <p>Tasty</p>
+          <p>Food</p>
+          <p>Recipes</p>
+          <span>Guaranteed</span> */}
+        <h1>
+          Tasty Food Recipes <span>Guaranteed</span>
+        </h1>
+      </section>
     </>
-  )
-}
+  );
+};
 
 export default Home;
