01c19053cf5260dd8893cd3bf1b715b1617d4427
Pavitrank <pavitrank2002@gmail.com>
2025-05-10 10:39:58 +0530

    "feat: Added new reponsive component Working"

--- src/Components/Working.jsx
+++ src/Components/Working.jsx
@@ -6,6 +6,55 @@ const Working = () => {
     <>
       <section className="workingContainer">
         <h1>Our Working</h1>
+        <section className="timeline">
+          <section className="timelineContainer leftContainer">
+            <section className="textBox">
+              <h2>Search a Recipe</h2>
+              <p>
+                Search through a wide variety of recipes across cuisines,
+                ingredients, and flavors to find the perfect dish for any
+                occasion.
+              </p>
+            </section>
+          </section>
+          <section className="timelineContainer rightContainer">
+            <section className="textBox">
+              <h2>Bookmark the Recipe</h2>
+              <p>
+                Save your favourite dishes for quick access anytime with just
+                one click.
+              </p>
+            </section>
+          </section>
+          <section className="timelineContainer leftContainer">
+            <section className="textBox">
+              <h2>Increase the serving</h2>
+              <p>
+                Adjust the number of servings to automatically scale all
+                ingredients, making it easy to cook.
+              </p>
+            </section>
+          </section>
+          <section className="timelineContainer rightContainer">
+            <section className="textBox">
+              <h2>Add a new Recipe</h2>
+              <p>
+                Create and add your own delicious recipe from scratch—customize
+                ingredients, steps, and make it uniquely yours to share or save
+                for later
+              </p>
+            </section>
+          </section>
+          <section className="timelineContainer leftContainer">
+            <section className="textBox">
+              <h2>Get directions for cooking</h2>
+              <p>
+                Follow step-by-step cooking directions to prepare your recipe
+                with ease and confidence, from prep to plating
+              </p>
+            </section>
+          </section>
+        </section>
       </section>
     </>
   );
