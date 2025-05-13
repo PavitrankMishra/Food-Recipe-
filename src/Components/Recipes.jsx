b15da0248827cac18331f486cc997697184c1849
Pavitrank <pavitrank2002@gmail.com>
2025-05-13 11:12:08 +0530

    "feat: Created a header for recipe container"

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import React from "react";
+import Header from "./Header";
+import "./Recipes.css";
+// import {useState, useEffect} from "react";
+import Logo from "../Assets/Logo.png";
 
-const Recipes = () => {
+const Recipes = ({ data }) => {
+  // console.log(data);
   return (
     <>
-      
+      <Header />
+      <section className="recipeContainer">
+        <section className="recipeHeadingContainer">
+          <section className="containerLeft">
+            <img src={Logo} className="headingLogo" />
+            <input type="text" placeholder="Search over 1,000,000 recipes" />
+          </section>
+          <section className="containerRight">
+            <p>Add Recipe</p>
+            <p>Bookmark Recipe</p>
+          </section>
+        </section>
+      </section>
     </>
-  )
-}
+  );
+};
 
 export default Recipes;
