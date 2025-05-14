76d9c245f3f98c057f8fe2b08b96c58b32eb75f2
Pavitrank <pavitrank2002@gmail.com>
2025-05-14 17:31:51 +0530

    "feat: Developed the based structure for recipe component"

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -8,6 +8,14 @@ import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import { faBookmark } from "@fortawesome/free-regular-svg-icons";
+import { faClock } from "@fortawesome/free-regular-svg-icons";
+// import { faPerson } from "@fortawesome/free-solid-svg-icons";
+import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import a from "../Assets/VegPizza.jpg";
+import { faMinus } from "@fortawesome/free-solid-svg-icons";
+// import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 const Recipes = ({ data }) => {
   // console.log(data);
@@ -114,7 +122,67 @@ const Recipes = ({ data }) => {
               </section>
             </section>
           </section>
-          <section className="recipeMiddleRight">This is right part</section>
+          <section className="recipeMiddleRight">
+            <section className="sec-1">
+              <img src={a} alt="Something" />
+              <p>Something</p>
+            </section>
+            <section className="sec-2">
+              <section className="secInner">
+                <section className="clockContainer">
+                  <FontAwesomeIcon icon={faClock} className="clock" />
+                  <p>60 Minutes</p>
+                </section>
+                <section className="userContainer">
+                  <FontAwesomeIcon icon={faUser} className="user" />
+                  <p>
+                    <span>4</span> Servings
+                  </p>
+                  <FontAwesomeIcon icon={faMinus} className="minus" />
+                  <FontAwesomeIcon icon={faPlus} className="plus" />
+                </section>
+              </section>
+              <section className="secOuter">
+                <section className="likeContainer">
+                  <FontAwesomeIcon icon={faHeart} className="heart" />
+                </section>
+              </section>
+            </section>
+            <section className="sec-3">
+              <h2>Recipe Ingredients</h2>
+              <ul className="listContainer">
+                <li>
+                  <FontAwesomeIcon icon={faCircleCheck} className="check" />
+                  <p>400 can chickpeas, rinsed and drained</p>
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCircleCheck} className="check" />
+                  <p>400 can chickpeas, rinsed and drained</p>
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCircleCheck} className="check" />
+                  <p>400 can chickpeas, rinsed and drained</p>
+                </li>
+                <li>
+                  <FontAwesomeIcon icon={faCircleCheck} className="check" />
+                  <p>400 can chickpeas, rinsed and drained</p>
+                </li>
+              </ul>
+              <button className="addToCart">
+                <FontAwesomeIcon icon={faCartShopping} />
+                ADD TO SHOPPING LIST
+              </button>
+            </section>
+            <section className="sec-4">
+              <h1>HOW TO COOK IT</h1>
+              <p>
+                The recipe was carefully designed and tested by{" "}
+                <stron>BBC Good Food.</stron>
+              </p>
+              <p>Please check out directions at their website.</p>
+              <button>DIRECTIONS</button>
+            </section>
+          </section>
         </section>
       </section>
     </>
