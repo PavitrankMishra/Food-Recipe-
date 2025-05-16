296a219c4f581443e060431b8b09aaf8eac93808
Pavitrank <pavitrank2002@gmail.com>
2025-05-16 13:38:11 +0530

    "feat: Implemented recipe upgradation based on servings"

--- src/Components/Recipes.jsx
+++ src/Components/Recipes.jsx
@@ -1,88 +1,75 @@
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import "./Recipes.css";
-// import {useState, useEffect} from "react";
 import Logo from "../Assets/Logo.png";
-import Pasta from "../Assets/Pasta.jpg";
-import { faUser } from "@fortawesome/free-regular-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faBookmark } from "@fortawesome/free-regular-svg-icons";
-import { faClock } from "@fortawesome/free-regular-svg-icons";
-// import { faPerson } from "@fortawesome/free-solid-svg-icons";
-import { faHeart } from "@fortawesome/free-regular-svg-icons";
-import a from "../Assets/vegPizza.jpg";
-import { faMinus } from "@fortawesome/free-solid-svg-icons";
-// import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
-import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
+import {
+  faUser,
+  faBookmark,
+  faClock,
+  faHeart,
+  faCircleCheck,
+} from "@fortawesome/free-regular-svg-icons";
+import {
+  faPlus,
+  faMinus,
+  faCartShopping,
+  faArrowRightLong,
+} from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import { useRef } from "react";
 
 const Recipes = ({ data, recipe, getData, getId }) => {
-  let [res_Per_Page, setResults_Per_Page] = useState(10);
-  let [page, setPage] = useState(1);
-  let startIndex = (page - 1) * res_Per_Page;
-  let endIndex = page * res_Per_Page;
-  const currentData = data?.slice(startIndex, endIndex);
-  if (data) {
-    console.log(data);
-    console.log(data.length);
-  }
-
-  const countRef = useRef(0);
-
-  // useEffect(() => {
-  //   count.current = count.current + 1;
-  // }, []);
-
-  console.log(countRef.current);
-  function incrementCount() {
-    if (+countRef.current.innerText >= 0) {
-      countRef.current.innerText = +countRef.current.innerText + 1;
-    }
-  }
-
-  function decrementCount() {
-    if (+countRef.current.innerText > 0) {
-      countRef.current.innerText = +countRef.current.innerText - 1;
-    }
-  }
-
-  function handlePreviousList() {
-    console.log("Handle Prev List Clicked");
-    setPage((p) => p - 1);
-    console.log(page);
-  }
-
-  function handleNextList() {
-    console.log("Handle NExt Lit clicked");
-    setPage((p) => p + 1);
-    console.log(page);
-  }
-  let d1 = [{ ...recipe }];
-
+  const [resPerPage, setResPerPage] = useState(10);
+  const [page, setPage] = useState(1);
   const [inputValue, setInputValue] = useState("Pizza");
-  const [inputId, setInputId] = useState("");
+  const [servings, setServings] = useState(recipe?.servings || 4); // default 4 servings
+
+  const startIndex = (page - 1) * resPerPage;
+  const endIndex = page * resPerPage;
+  const currentData = data?.slice(startIndex, endIndex);
 
   useEffect(() => {
     getData(inputValue);
-    // console.log(inputValue);
   }, [inputValue]);
 
-  function sendID(id) {
-    console.log(id);
+  const handlePreviousList = () => {
+    setPage((p) => p - 1);
+  };
+
+  const handleNextList = () => {
+    setPage((p) => p + 1);
+  };
+
+  const sendID = (id) => {
     getId(id);
-  }
+    setServings(recipe?.servings || 4); // Reset to default when a new recipe is selected
+  };
+
+  const incrementCount = () => {
+    setServings((prev) => prev + 1);
+  };
+
+  const decrementCount = () => {
+    setServings((prev) => (prev > 1 ? prev - 1 : 1));
+  };
+
+  const updatedIngredients =
+    recipe?.ingredients?.map((ing) => {
+      const factor = servings / (recipe?.servings || 4);
+      return {
+        ...ing,
+        quantity: ing.quantity ? (ing.quantity * factor).toFixed(2) : null,
+      };
+    }) || [];
 
   return (
     <>
       <Header />
       <section className="recipeContainer">
+        {/* Top search and actions */}
         <section className="recipeHeadingContainer">
           <section className="containerLeft">
-            <img src={Logo} className="headingLogo" />
+            <img src={Logo} className="headingLogo" alt="logo" />
             <input
               type="text"
               placeholder="Search over 1,000,000 recipes"
@@ -98,130 +85,133 @@ const Recipes = ({ data, recipe, getData, getId }) => {
           </section>
         </section>
 
+        {/* Main content */}
         <section className="recipeMiddleContainer">
+          {/* Left Side: List of Recipes */}
           <section className="recipeMiddleLeft">
-            <section>
-              {currentData && currentData.length > 0 ? (
-                currentData.map((item, index) => (
-                  <>
-                    <section
-                      className="recipeSection"
-                      key={index}
-                      onClick={() => sendID(item.id)}
-                    >
-                      <section className="recipe1">
-                        <section className="recipeImage">
-                          <img src={item.image_url} alt={item.title} />
-                        </section>
-                        <section className="recipeInner">
-                          <p id="itemName">{item.title}</p>
-                          <p id="personName">{item.publisher}</p>
-                        </section>
-                      </section>
-                      <section className="userSection">
-                        <FontAwesomeIcon icon={faUser} />
-                      </section>
-                    </section>
-                  </>
-                ))
-              ) : (
-                <p>There is no data</p>
-              )}
-              <section className="buttonContainer">
-                <button
-                  className="butonLeft"
-                  onClick={handlePreviousList}
-                  disabled={page === 1}
+            {currentData && currentData.length > 0 ? (
+              currentData.map((item) => (
+                <section
+                  className="recipeSection"
+                  key={item.id}
+                  onClick={() => sendID(item.id)}
                 >
-                  Prev
-                </button>
-                <button
-                  className="buttonRight"
-                  onClick={handleNextList}
-                  disabled={page >= Math.ceil(data?.length / res_Per_Page)}
-                >
-                  Next
-                </button>
-              </section>
-            </section>
-          </section>
-
-          <section className="recipeMiddleRight">
-            {d1 && d1.length > 0 ? (
-              d1.map((i, index) => (
-                <>
-                  <section className="sec-1">
-                    <img src={i.image_url} alt="Something" />
-                    <p>Something</p>
-                  </section>
-                  <section className="sec-2">
-                    <section className="secInner">
-                      <section className="clockContainer">
-                        <FontAwesomeIcon icon={faClock} className="clock" />
-                        <p>{i.cooking_time} minutes</p>
-                      </section>
-                      <section className="userContainer">
-                        <FontAwesomeIcon icon={faUser} className="user" />
-                        <p>
-                          <span ref={countRef}>4</span> Servings
-                        </p>
-                        <FontAwesomeIcon
-                          icon={faMinus}
-                          className="minus"
-                          onClick={decrementCount}
-                        />
-                        <FontAwesomeIcon
-                          icon={faPlus}
-                          className="plus"
-                          onClick={incrementCount}
-                        />
-                      </section>
+                  <section className="recipe1">
+                    <section className="recipeImage">
+                      <img src={item.image_url} alt={item.title} />
                     </section>
-                    <section className="secOuter">
-                      <section className="likeContainer">
-                        <FontAwesomeIcon icon={faHeart} className="heart" />
-                      </section>
+                    <section className="recipeInner">
+                      <p id="itemName">{item.title}</p>
+                      <p id="personName">{item.publisher}</p>
                     </section>
                   </section>
-                  <section className="sec-3">
-                    <h2>Recipe Ingredients</h2>
-
-                    {i.ingredients && i.ingredients.length > 0 ? (
-                      <ul className="listContainer">
-                        {i.ingredients.map((ig, index) => (
-                          <li key={index}>
-                            <FontAwesomeIcon
-                              icon={faCircleCheck}
-                              className="check"
-                            />
-                            <p>{`${ig.quantity} ${ig.unit} ${ig.description}`}</p>
-                          </li>
-                        ))}
-                      </ul>
-                    ) : (
-                      <p>There are no ingredients listed.</p>
-                    )}
-
-                    <button className="addToCart">ADD TO SHOPPING LIST</button>
+                  <section className="userSection">
+                    <FontAwesomeIcon icon={faUser} />
                   </section>
-
-                  <section className="sec-4">
-                    <h1>HOW TO COOK IT</h1>
-                    <p>
-                      The recipe was carefully designed and tested by{" "}
-                      <strong>{i.publisher}.</strong>
-                    </p>
-                    <p>Please check out directions at their website.</p>
-                    <Link to={i.source_url}>
-                      <button className="directions">
-                        DIRECTIONS <FontAwesomeIcon icon={faArrowRightLong} />
-                      </button>
-                    </Link>
-                  </section>
-                </>
+                </section>
               ))
             ) : (
               <p>There is no data</p>
+            )}
+
+            {/* Pagination */}
+            <section className="buttonContainer">
+              <button
+                className="butonLeft"
+                onClick={handlePreviousList}
+                disabled={page === 1}
+              >
+                Prev
+              </button>
+              <button
+                className="buttonRight"
+                onClick={handleNextList}
+                disabled={page >= Math.ceil(data?.length / resPerPage)}
+              >
+                Next
+              </button>
+            </section>
+          </section>
+
+          {/* Right Side: Selected Recipe Details */}
+          <section className="recipeMiddleRight">
+            {recipe ? (
+              <>
+                <section className="sec-1">
+                  <img src={recipe.image_url} alt={recipe.title} />
+                  <p>{recipe.title}</p>
+                </section>
+
+                <section className="sec-2">
+                  <section className="secInner">
+                    <section className="clockContainer">
+                      <FontAwesomeIcon icon={faClock} className="clock" />
+                      <p>{recipe.cooking_time} minutes</p>
+                    </section>
+
+                    <section className="userContainer">
+                      <FontAwesomeIcon icon={faUser} className="user" />
+                      <p>{servings} Servings</p>
+                      <FontAwesomeIcon
+                        icon={faMinus}
+                        className="minus"
+                        onClick={decrementCount}
+                      />
+                      <FontAwesomeIcon
+                        icon={faPlus}
+                        className="plus"
+                        onClick={incrementCount}
+                      />
+                    </section>
+                  </section>
+
+                  <section className="secOuter">
+                    <section className="likeContainer">
+                      <FontAwesomeIcon icon={faHeart} className="heart" />
+                    </section>
+                  </section>
+                </section>
+
+                <section className="sec-3">
+                  <h2>Recipe Ingredients</h2>
+                  {updatedIngredients.length > 0 ? (
+                    <ul className="listContainer">
+                      {updatedIngredients.map((ig, index) => (
+                        <li key={index}>
+                          <FontAwesomeIcon
+                            icon={faCircleCheck}
+                            className="check"
+                          />
+                          <p>{`${ig.quantity || ""}`}</p>
+                          <p>&nbsp;{`${ig.unit} ${ig.description}`}</p>
+                        </li>
+                      ))}
+                    </ul>
+                  ) : (
+                    <p>There are no ingredients listed.</p>
+                  )}
+                  <button className="addToCart">
+                    <FontAwesomeIcon icon={faCartShopping} /> ADD TO SHOPPING
+                    LIST
+                  </button>
+                </section>
+
+                <section className="sec-4">
+                  <h1>HOW TO COOK IT</h1>
+                  <p>
+                    The recipe was carefully designed and tested by{" "}
+                    <strong>{recipe.publisher}</strong>.
+                  </p>
+                  <p>Please check out directions at their website.</p>
+                  <Link to={recipe.source_url} target="_blank">
+                    <button className="directions">
+                      DIRECTIONS <FontAwesomeIcon icon={faArrowRightLong} />
+                    </button>
+                  </Link>
+                </section>
+              </>
+            ) : (
+              <p>Select a recipe to view details</p>
             )}
           </section>
         </section>
