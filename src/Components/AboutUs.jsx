b460f047c03fcb9196d236634fc1f319721dfb3a
Pavitrank <pavitrank2002@gmail.com>
2025-05-21 09:25:10 +0530

    "feat: Implemented typewriting effect"

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../Components/AboutUs.css";
 import vegPizza from "../Assets/vegPizza.jpg";
 import { Link } from "react-router-dom";
+import Typewriter from "typewriter-effect";
 
 const AboutUs = () => {
   return (
@@ -9,7 +10,18 @@ const AboutUs = () => {
       <section className="aboutContainer">
         <section className="left1">
           <h1>About Us</h1>
-          <p>Discover the authentic flavors of chinese cuisine foods.</p>
+          <p>Discover the authentic flavors of</p>
+          <section className="special">
+            <Typewriter
+              options={{
+                strings: ["Chinese", "Italian", "Spanish"],
+                autoStart: true,
+                loop: true,
+                cursor: "",
+              }}
+            />{" "}
+          </section>
+          <p>cuisine foods.</p>
           <section className="para">
             <span>
               Experience the rich and diverse flavors of Chinese cuisine, where
