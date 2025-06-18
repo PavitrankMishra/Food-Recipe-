c34d582a3aa239ca5be4d3b7294094d5777bcab3
Pavitrank <pavitrank2002@gmail.com>
2025-06-18 23:26:00 +0530

    "style:Updated layout to include style and button on left part of Hero component"

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import "./Hero.css";
 import Hero1 from "../Assets/Hero1.png";
+import Button from "./Button";
 
 const Hero = () => {
   return (
     <>
       <section className="heroContainer">
-        <section className="heroLeft"></section>
+        <section className="heroLeft">
+          <h1>Recipes with a</h1>
+          <span>Personal Touch</span>
+          <p>
+            Not just ingredients — share your memories, notes, and twists with
+            every dish. Your kitchen, your story.
+          </p>
+          <Button location={"/recipes"}>Explore Recipes</Button>
+        </section>
         <section className="heroRight">
           <img src={Hero1} alt="Hero Image" />
         </section>
