5ebd7bbc2d8b952a2ece3bb9940c4966c898534c
Pavitrank <pavitrank2002@gmail.com>
2025-06-28 22:35:56 +0530

    "style: styled the user interface for hero component"

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -2,23 +2,41 @@ import React from "react";
 import "./Hero.css";
 import Hero1 from "../Assets/Hero1.png";
 import Button from "./Button";
+import HeroRight from "../Assets/HeroRight.png";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Hero = () => {
   return (
     <>
       <section className="heroContainer">
         <section className="heroLeft">
-          <h1>RECIPES</h1>
-          <h1>WITH A</h1>
-          <span>PERSONAL TOUCH</span>
-          <p>
-            Not just ingredients — share your memories, notes, and twists with
-            every dish. Your kitchen, your story.
-          </p>
-          <Button location={"/recipes"}>Explore Recipes</Button>
+          <section className="leftText">
+            <h1>Experience the</h1>
+            <h1>
+              <span>Comfort </span> of
+            </h1>
+            <h1>Homemade Cooking</h1>
+          </section>
+          <span className="subtext">
+            Forkify is the best website to explore new Snacks and recipes of
+            food and cakes it is very simple to use and the best way to find
+            something new with the help of us
+          </span>
+          <section className="inputRecipe">
+            <input
+              type="text"
+              value="Falafel Burgers"
+              className="styled-input"
+            />
+            <section className="magnifyingContainer">
+              <FontAwesomeIcon icon={faMagnifyingGlass} size="2xl" />
+            </section>
+          </section>
+          {/* <Button location={"/recipes"}>Explore Recipes</Button> */}
         </section>
         <section className="heroRight">
-          <img src={Hero1} alt="Hero Image" />
+          <img src={HeroRight} alt="Hero Image" />
         </section>
       </section>
     </>
